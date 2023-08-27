e7080aa4733b5198caf1fd93d2f7922cf85a09b7
Muthuinc <smramalingamm@gmail.com>
2023-08-27 16:18:34 +0530

    echo added

--- ansible/ansible.sh
+++ ansible/ansible.sh
@@ -5,6 +5,8 @@
 a=$(aws ec2 describe-instances --region ap-southeast-2 --filters "Name=tag:Name,Values=app1" --query 'Reservations[].Instances[].PublicIpAddress' --output text)
 # store the ip address of the instance
 
+echo $a
+
 sed -i "s/muthuu/$a/g" inventory.txt
 # inserting the dynamically created ip to the inventory.txt file 
 

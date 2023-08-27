d699149dd702cb1906b1ceee54b78c33703ad12c
Muthu ramalingam <128946991+Muthuinc@users.noreply.github.com>
2023-08-27 16:41:56 +0530

    Update ansible.sh

--- ansible/ansible.sh
+++ ansible/ansible.sh
@@ -7,6 +7,8 @@ a=$(aws ec2 describe-instances --region ap-southeast-2 --filters "Name=tag:Name,
 
 echo $a
 
-sed -i "s/muthuu/$a/g" inventory.txt
+ansible-playbook --private-key=$Muthu --user=ubuntu --extra-vars="ansible_host=$a" ansible.yml
+
+#sed -i "s/muthuu/$a/g" inventory.txt
 # inserting the dynamically created ip to the inventory.txt file 
 
